8ce3a748487fb73aac42b3269cce1072347f3dae
Amine Fajry <amine.f@dalvin.fr>
2024-02-19 19:04:56 +0100

    Update quiz.service.ts

--- server/src/quiz/quiz.service.ts
+++ server/src/quiz/quiz.service.ts
@@ -1,26 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import OpenAI from "openai";
 @Injectable()
 export class QuizService {
-
-  constructor(private readonly openai: OpenAI) {
-  }
-
-  // Service pour la traduction des messages de la discussion
-  async makeTranslate(text: string, language: string): Promise<string> {
-    let data;
-    try {
-      data = await this.openai.chat.completions.create({
-        messages: [{role: 'user', content: `Traduit ce text: '${text}' en '${language}' sans commentaire`}],
-        model: 'gpt-3.5-turbo',
-      });
-
-      return data.choices[0].message.content;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-
-
+  constructor() {}
 }
